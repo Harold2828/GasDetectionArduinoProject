eae81aebe6a2e22ef4588667b2a2db49aa12aa48
Harold Ochoa <59937857+Harold2828@users.noreply.github.com>
2024-10-19 17:49:24 -0500

    Update claro

--- sketch_oct2a.ino
+++ sketch_oct2a.ino
@@ -3,30 +3,29 @@
 
 const int MQ2_PIN = A5;
 const int MQ135_PIN = A2;
-SoftwareSerial gsm(8, 9);
+SoftwareSerial gsm(7, 8); // RX, TX
 String phoneNumber = "+573138133118";
-
 const int ARRAY_SIZE = 10;
 String sensorDataArray[ARRAY_SIZE];
 
-// GSM setup function
 void setupGSM() {
   gsm.begin(9600);
   delay(1000);
   Serial.println("Initializing GSM module...");
   gsm.println("AT");
   delay(1000);
-  gsm.println("AT+CMGF=1"); // Set SMS mode
+  gsm.println("AT+CMGF=1");
   delay(1000);
-  gsm.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\""); // Set GPRS mode
+  gsm.println("AT+CGNSPWR=1"); // Power on GPS
   delay(1000);
-  gsm.println("AT+SAPBR=3,1,\"APN\",\"internet.comcel.com.co\""); // Replace with your APN
+  gsm.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\""); // Set connection type to GPRS
   delay(1000);
-  gsm.println("AT+SAPBR=1,1"); // Activate GPRS context
-  delay(3000); // Wait for the connection to establish
+  gsm.println("AT+SAPBR=3,1,\"APN\",\"internet.comcel.com.co\""); // Set your APN (replace "your_apn" with your SIM's APN)
+  delay(1000);
+  gsm.println("AT+SAPBR=1,1"); // Open GPRS context
+  delay(3000);
 }
 
-// Send SMS function
 void sendSMS(String message) {
   gsm.print("AT+CMGS=\"");
   gsm.print(phoneNumber);
@@ -34,12 +33,11 @@ void sendSMS(String message) {
   delay(1000);
   gsm.print(message);
   delay(1000);
-  gsm.write(26); // ASCII code for Ctrl+Z to send SMS
+  gsm.write(26);
   delay(1000);
   Serial.println("SMS sent: " + message);
 }
 
-// Read sensor function
 float readSensor(int pin, String sensorType) {
   int sensorValue = analogRead(pin);
   float voltage = sensorValue * (5.0 / 1023.0);
@@ -60,18 +58,56 @@ float readSensor(int pin, String sensorType) {
   return voltage * 100;
 }
 
-// Get GPS coordinates function
+void postToServer(String jsonBody) {
+  // Start HTTP session
+  gsm.println("AT+HTTPINIT");
+  delay(1000);
+
+  // Set HTTP parameters
+  gsm.println("AT+HTTPPARA=\"CID\",1");
+  delay(1000);
+  gsm.println("AT+HTTPPARA=\"URL\",\"https://gbuitrago.com/backend/app/service/V1/api.php\"");
+  delay(1000);
+  gsm.println("AT+HTTPPARA=\"CONTENT\",\"application/json\"");
+  delay(1000);
+
+  // Send data length
+  gsm.print("AT+HTTPDATA=");
+  gsm.print(jsonBody.length());
+  gsm.println(",10000");
+  delay(1000);
+
+  // Send the actual JSON data
+  gsm.println(jsonBody);
+  delay(1000);
+
+  // Start HTTP POST
+  gsm.println("AT+HTTPACTION=1");
+  delay(5000); // Wait for server response
+
+  // Read server response
+  gsm.println("AT+HTTPREAD");
+  delay(1000);
+  while (gsm.available()) {
+    Serial.write(gsm.read()); // Print server response to Serial monitor
+  }
+
+  // Terminate HTTP session
+  gsm.println("AT+HTTPTERM");
+  delay(1000);
+}
+
 void getCoordinates(float &latitude, float &longitude) {
-  gsm.println("AT+CGNSINF"); // Get GNSS information
+  gsm.println("AT+CGNSINF"); // Send command to get GNSS information
   delay(1000);
   String gpsData = "";
   while (gsm.available()) {
     char c = gsm.read();
     gpsData += c;
   }
-  Serial.println("GPS Data: " + gpsData);
 
-  int latStart = gpsData.indexOf(',') + 3;
+  // Parse the response
+  int latStart = gpsData.indexOf(',') + 3; // Skip first two commas
   int latEnd = gpsData.indexOf(',', latStart);
   latitude = gpsData.substring(latStart, latEnd).toFloat();
 
@@ -85,48 +121,6 @@ void getCoordinates(float &latitude, float &longitude) {
   Serial.println(longitude);
 }
 
-// Function to send POST request
-void sendPostRequest(float mq2PPM, float mq135PPM, float latitude, float longitude) {
-  // Start HTTP session
-  gsm.println("AT+HTTPINIT");
-  delay(1000);
-
-  // Set the URL for POST
-  gsm.println("AT+HTTPPARA=\"URL\",\"https://gbuitrago.com/backend/app/service/V1/api.php\"");
-  delay(1000);
-
-  // Set the content type to application/json
-  gsm.println("AT+HTTPPARA=\"CONTENT\",\"application/json\"");
-  delay(1000);
-
-  // Build JSON data
-  String jsonBody = "[{\"sensor_id\":1,\"value_sensor\":" + String(mq2PPM) + ",\"feature_sensor\":\"Parts per million\",\"created_by\":1,\"updated_by\":1}]";
-  
-  // Set POST data size
-  gsm.println("AT+HTTPDATA=" + String(jsonBody.length()) + ",10000");
-  delay(1000);
-
-  // Send POST data
-  gsm.print(jsonBody);
-  delay(5000); // Wait for the data to be uploaded
-
-  // Send POST request
-  gsm.println("AT+HTTPACTION=1");
-  delay(5000); // Wait for response
-
-  // Read response
-  gsm.println("AT+HTTPREAD");
-  delay(1000);
-  while (gsm.available()) {
-    char c = gsm.read();
-    Serial.print(c);
-  }
-
-  // End HTTP session
-  gsm.println("AT+HTTPTERM");
-  delay(1000);
-}
-
 void setup() {
   Serial.begin(9600);
   setupGSM();
@@ -138,38 +132,14 @@ void setup() {
 void loop() {
   float mq2PPM = readSensor(MQ2_PIN, "MQ2");
   float mq135PPM = readSensor(MQ135_PIN, "MQ135");
-
   float latitude, longitude;
   getCoordinates(latitude, longitude);
 
-  // Store data in JSON format
-  StaticJsonDocument<200> doc;
-  doc["mq2"] = mq2PPM;
-  doc["mq135"] = mq135PPM;
-  doc["latitude"] = latitude;
-  doc["longitude"] = longitude;
+  // Prepare JSON body to be sent
+  String jsonBody = "[{\"sensor_id\":1,\"value_sensor\":100,\"feature_sensor\":\"Parts per millioin\",\"created_by\":1,\"updated_by\":1}]";
+  
+  // Post the JSON body to the server
+  postToServer(jsonBody);
 
-  String jsonString;
-  serializeJson(doc, jsonString);
-
-  for (int i = 0; i < ARRAY_SIZE - 1; i++) {
-    sensorDataArray[i] = sensorDataArray[i + 1];
-  }
-  sensorDataArray[ARRAY_SIZE - 1] = jsonString;
-
-  Serial.println("Sensor Data Array:");
-  Serial.println("[");
-  for (int i = 0; i < ARRAY_SIZE; i++) {
-    Serial.println(sensorDataArray[i]);
-    if (i < ARRAY_SIZE - 1) {
-      Serial.println(",");
-    }
-  }
-  Serial.println("]");
-
-  // Send the POST request
-  sendPostRequest(mq2PPM, mq135PPM, latitude, longitude);
-
-  delay(5000); // Wait 5 seconds before reading again
+  delay(60000); // Wait 60 seconds before posting again
 }
-
